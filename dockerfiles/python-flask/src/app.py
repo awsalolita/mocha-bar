16d154099fae4d90fd4cd5c0d174c6c1d9c363df
SalehBorhani <borhanisaleh6@gmail.com>
2026-08-12 14:07:34 +0330

    feat: add routes for flask

--- dockerfiles/python-flask/src/app.py
+++ dockerfiles/python-flask/src/app.py
@@ -7,6 +7,15 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+## middleware
+@app.before_request
+def require_api_key():
+    if request.path.startswith("/api"):
+        if request.path.startswith("/api/client"):
+            return None
+        if not request.headers.get("X-API-Key"):
+            return jsonify({"error": "unauthorized"}), 401
+
 @app.route("/")
 def hello():
   logger.info("hello endpoint hit")
