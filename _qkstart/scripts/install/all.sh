c6238e8d39e17c2ff6061e0f2bdd02cbb6dc1d98
SalehBorhani <borhanisaleh6@gmail.com>
2026-08-24 13:37:56 +0330

    feat: add more doscs

--- _qkstart/scripts/install/all.sh
+++ _qkstart/scripts/install/all.sh
@@ -7,7 +7,7 @@ for a in "$@"; do
   [ "$a" = "-y" ] && y="-y"
 done
 
-for pkg in basic zsh kubectl fzf eksctl k9s helm docker aws_autocomplete kubectx terraform alias; do
+for pkg in basic kubectl fzf eksctl k9s helm docker kubectx terraform alias; do
   echo "[all] installing $pkg"
   q i "$pkg" $y
 done
